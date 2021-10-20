f3cd95cc054486d3aa1fa90f8aa526f6f644c7b9
Cullie <cullie.02@gmail.com>
2021-10-20 16:58:31 +1100

    Comments for ciphers

--- Steganography/Shift.cpp
+++ Steganography/Shift.cpp
@@ -1,20 +1,20 @@
 #include <string>
-
-std::string shift_encode(std::string plain_text, unsigned int key)
+std::string shift_encode(std::string plain_text, unsigned int key)  // for encrypting string with shift cipher
 {
     char ch;
     for (int i = 0; i < plain_text.length(); ++i) {
         ch = plain_text[i];
-        //  for lower case
-        if (ch >= 'a' && ch <= 'z') {
+
+        if (ch >= 'a' && ch <= 'z') {   //  Add key to lower case plain text to convert it to cipher text
             ch = ch + key;
             if (ch > 'z') {
                 ch = ch - 'z' + 'a' - 1;
             }
             plain_text[i] = ch;
         }
-        //  for upper case
-        else if (ch >= 'A' && ch <= 'Z') {
+
+
+        else if (ch >= 'A' && ch <= 'Z') {   //  Add key to upper case plain text to convert it to cipher text
             ch = ch + key;
             if (ch > 'Z') {
                 ch = ch - 'Z' + 'A' - 1;
@@ -26,23 +26,22 @@ std::string shift_encode(std::string plain_text, unsigned int key)
 }
 
 
-// for Decryption
 
-
-std::string shift_decode(std::string cipher_text, unsigned int key) {
+std::string shift_decode(std::string cipher_text, unsigned int key) {  // for decrypting string with shift cipher
     char ch;
     for (int i = 0; i < cipher_text.length(); ++i) {
         ch = cipher_text[i];
-        //  for lower case
-        if (ch >= 'a' && ch <= 'z') {
+
+        if (ch >= 'a' && ch <= 'z') {  //  Subtracting key from lower case cipher text to convert it to plain text
             ch = ch - key;
             if (ch < 'a') {
                 ch = ch + 'z' - 'a' + 1;
             }
             cipher_text[i] = ch;
         }
-        //  for upper case
-        else if (ch >= 'A' && ch <= 'Z') {
+
+
+        else if (ch >= 'A' && ch <= 'Z') {  //  Subtracting key from upper case cipher text to convert it to plain text
             ch = ch - key;
             if (ch < 'A') {
                 ch = ch + 'Z' - 'A' + 1;
--- Steganography/Substitute.cpp
+++ Steganography/Substitute.cpp
@@ -1,5 +1,5 @@
 #include <string>
-std::string cipherText(std::string cipheralpha) {
+std::string cipherText(std::string cipheralpha) { //Converting string to uppercase
     for (int i = 0; cipheralpha[i] != '\0'; i++)
     {
         if (cipheralpha[i] >= 'a' && cipheralpha[i] <= 'z')
@@ -7,14 +7,14 @@ std::string cipherText(std::string cipheralpha) {
     }
 
     std::string ciphertext = "";
-    ciphertext = ciphertext + cipheralpha;
+    ciphertext = ciphertext + cipheralpha; // Appending sub_keyword before any other alphabet addition
     bool ch[26] = { false };
-    for (int i = 0; cipheralpha[i] != '\0'; i++)
+    for (int i = 0; cipheralpha[i] != '\0'; i++) // Marking all positions of sub_keyword in boolean array
     {
-        int diff = cipheralpha[i] - 65;
+        int diff = cipheralpha[i] - 65; // Calculate position of alphabet in array
         ch[diff] = true;
     }
-    for (int i = 0; i < 26; i++)
+    for (int i = 0; i < 26; i++) // Appending all other alphabets not present in sub_keyword
     {
         if (ch[i] == false)
         {
@@ -24,20 +24,22 @@ std::string cipherText(std::string cipheralpha) {
     }
     return ciphertext;
 }
-std::string substitute_encode(std::string plaintext, std::string sub_keyword) {
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    std::string ciphertext = cipherText(sub_keyword);
-    for (int i = 0; plaintext[i] != '\0'; i++)
+std::string substitute_encode(std::string plaintext, std::string sub_keyword) { // For encrypting string with substitute cipher
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // alphabet string to match against
+
+    std::string ciphertext = cipherText(sub_keyword); // Call cipherText to get string of cipheralphabets
+
+    for (int i = 0; plaintext[i] != '\0'; i++) // Convert plaintext to uppercase
     {
         if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
             plaintext[i] = plaintext[i] - 32;
     }
     std::string encryption;
-    for (char character : plaintext) {
+    for (char character : plaintext) { // Obtain characters in plaintext and find corresponding character in ciphertext string
         size_t position = alphabet.find(character);
         if (position != std::string::npos) {
             char new_char{ ciphertext.at(position) };
-            encryption += new_char;
+            encryption += new_char; // Appending cipher characters to create cipher text
         }
         else {
             encryption += character;
@@ -46,21 +48,23 @@ std::string substitute_encode(std::string plaintext, std::string sub_keyword) {
     return encryption;
 }
 
-std::string substitute_decode(std::string ciphertext, std::string sub_keyword)
+std::string substitute_decode(std::string ciphertext, std::string sub_keyword) // For decryption of string with substitute cipher
 {
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    std::string cipher_text = cipherText(sub_keyword);
-    for (int i = 0; ciphertext[i] != '\0'; i++)
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // alphabet string to match against
+
+    std::string cipher_text = cipherText(sub_keyword);  // Call cipherText to get string of cipheralphabets
+
+    for (int i = 0; ciphertext[i] != '\0'; i++) // Convert ciphertext to uppercase
     {
         if (ciphertext[i] >= 'a' && ciphertext[i] <= 'z')
             ciphertext[i] = ciphertext[i] - 32;
     }
     std::string decryption;
-    for (char character : ciphertext) {
+    for (char character : ciphertext) {  // Obtain characters in ciphertext and find corresponding character in alphabet string
         size_t position = cipher_text.find(character);
         if (position != std::string::npos) {
             char new_char{ alphabet.at(position) };
-            decryption += new_char;
+            decryption += new_char; // Appending alphabets characters to create plain text
         }
         else
             decryption += character;
